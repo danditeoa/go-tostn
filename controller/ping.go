8b4e8c4009c03e9abcddc7f418860d8d7cd87515
danditeoa <taci.dandi@gmail.com>
2020-02-23 23:55:36 -0300

    refactor(mvc): creating controller, changing the check api to ping.go, and creating route.go

---
+++ controller/ping.go
@@ -0,0 +1,20 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"go-tostn/views"
+)
+
+func ping() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			data := structs.Response {
+				Code: http.StatusOK,
+				Body: "pong",
+			}
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(data)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-    "encoding/json"
     "net/http"
 
-    "go-tostn/views"
+    "go-tostn/controller"
 )
 
 func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-       if r.Method == http.MethodGet {
-           data := structs.Response {
-               Code: http.StatusOK,
-               Body: "pong",
-           }
-           json.NewEncoder(w).Encode(data)
-       }
-    })
+    mux := controller.Register()
     http.ListenAndServe(":3000", mux)
 }
