c87f68c6452ae621819d58f987140bc179b1f836
danditeoa <taci.dandi@gmail.com>
2020-02-28 01:03:30 -0300

    refactor(model): adding connection method with database to remove code repetition on accounts and transfers

--- model/accountsDAO.go
+++ model/accountsDAO.go
@@ -1,24 +1,16 @@
 package model
 
 import (
-	"database/sql"
 	structs "go-tostn/views"
-	"log"
 )
 
 func GetAccounts() []structs.Account {
-	db, err := sql.Open("mysql",
-		"root:senha123@tcp(127.0.0.1:3306)/gotostndb")
-	if err != nil {
-		log.Fatal(err)
-	}
-	gotostndb, err := db.Query("SELECT * FROM Accounts")
+	gotostndb := runQuery("SELECT * FROM Accounts")
 	arrayAccount := []structs.Account{}
 	for gotostndb.Next() {
 		var acct = structs.Account{}
 		gotostndb.Scan(&acct.Id, &acct.Name, &acct.Cpf, &acct.Balance, &acct.CreatedAt)
 		arrayAccount = append(arrayAccount, acct)
 	}
-	defer db.Close()
 	return arrayAccount
 }
--- model/transfersDAO.go
+++ model/transfersDAO.go
@@ -1,24 +1,16 @@
 package model
 
 import (
-	"database/sql"
 	structs "go-tostn/views"
-	"log"
 )
 
 func GetTransfers() []structs.Transfers {
-	db, err := sql.Open("mysql",
-		"root:senha123@tcp(127.0.0.1:3306)/gotostndb")
-	if err != nil {
-		log.Fatal(err)
-	}
-	gotostndb, err := db.Query("SELECT * FROM Transfers")
-	listTransfers := []structs.Transfers{}
+	gotostndb := runQuery("SELECT * FROM Transfers")
+	var listTransfers []structs.Transfers
 	for gotostndb.Next() {
 		var transfers = structs.Transfers{}
 		gotostndb.Scan(&transfers.Id, &transfers.AccountOriginId, &transfers.AccountDestinationId, &transfers.Amount, &transfers.CreatedAt)
 		listTransfers = append(listTransfers, transfers)
 	}
-	defer db.Close()
 	return listTransfers
 }
